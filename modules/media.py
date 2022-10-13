d465dfcaa399ee0c8dcbe80db4530e895e948fe8
molixmoli <molixmoli@gmail.com>
2022-10-13 08:33:47 +0200

    Primera versión

---
+++ modules/media.py
@@ -0,0 +1,205 @@
+import os
+from tkinter import Image
+from fpdf import FPDF
+from PIL import Image, ImageDraw, ImageFont
+from docx import Document
+import cv2
+
+class Media:
+
+    def generate_big_random_bin_file(self, filename, size):
+        """
+        generate big binary file with the specified size in bytes
+        :param filename: the filename
+        :param size: the size in bytes
+        :return:void
+        """
+        import os
+        with open('%s' % filename, 'wb') as fout:
+            fout.write(os.urandom(size))  # 1
+        #print('big random binary file with size %f generated ok' % size)
+        pass
+
+
+    def generate_big_random_letters(self, filename, size):
+        """
+        generate big random letters/alphabets to a file
+        :param filename: the filename
+        :param size: the size in bytes
+        :return: void
+        """
+        import random
+        import string
+
+        chars = ''.join([random.choice(string.ascii_letters) for i in range(size)])  # 1
+
+        with open(filename, 'w') as f:
+            f.write(chars)
+        pass
+
+
+    def generate_big_sparse_file(self, filename, size):
+        f = open(filename, "wb")
+        f.seek(size - 1)
+        f.write(b"\1")
+        f.close()
+        pass
+
+
+    def generate_big_random_sentences(self, filename, linecount):
+        import random
+
+        reg1 = ("Mami", "Gata", "Perra", "Zorra", "Chica")
+        reg2 = ("yo quiero", "vamos a", "yo voy a", "yo quiero", "yo vengo a")
+        reg3 = ("castigarte", "cogerte", "encenderte", "darte", "azotarte")
+        reg4 = ("duro", "rápido", "lento", "suave", "fuerte")
+        reg5 = ("hasta que salga el sol", "toda la noche", "hasta el amanecer", "hasta mañana", "todo el día")
+        reg6 = ("sin miedo", "sin anestesia", "en el piso", "contra la pared", "sin compromiso")
+
+        #all = [reg1, reg2, reg3, reg4, reg5, reg6]
+
+        col1 = ("Queridos compañeros", "Por otra parte,y dados los condicionamientos actuales", "Asimismo,",
+                "Sin embargo no hemos de olvidar que", "De igual manera,", "La práctica de la vida cotidiana prueba que,",
+                "No es indispensable argumentar el peso y la significación de estos problemas ya que,",
+                "Las experiencias ricas y diversas muestran que,", "El afán de organización, pero sobre todo",
+                "Los superiores principios ideológicos, condicionan que", "Incluso, bien pudiéramos atrevernos a sugerir que",
+                "Es obvio señalar que,", "Pero pecaríamos de insinceros si soslayásemos que,",
+                "Y además, quedaríamos inmersos en la más abyecta de las estulticias si no fueramos consacientes de que,",
+                "Por último, y como definitivo elemento esclarecedor, cabe añadir que,")
+        col2 = ("la realización de las premisas del programa", "la complejidad de los estudios de los dirigentes",
+                "el aumento constante, en cantidad y en extensión, de nuestra actividad", "la estructura actual de la organización",
+                "el nuevo modelo de actividad de la organización,", "el desarrollo continuo de distintas formas de actividad",
+                "nuestra actividad de información y propaganda", "el reforzamiento y desarrollo de las estructuras",
+                "la consulta con los numerosos militantes", "el inicio de la acción general de formación de las actitudes",
+                "un relanzamiento específico de todos los sectores implicados", "la superación de experiencias periclitadas",
+                "una aplicación indiscriminada de los factores confluyentes", "la condición sine qua non rectora del proceso",
+                "el proceso consensuado de unas y otras aplicaciones concurrentes")
+        col3 = ("nos obliga a un exhaustivo análisis", "cumple un rol escencial en la formación",
+                "exige la precisión y la determinación", "ayuda a la preparación y a la realización",
+                "garantiza la participación de un grupo importante en la formación", "cumple deberes importantes en la determinación",
+                "facilita la creación", "obstaculiza la apreciación de la importancia", "ofrece un ensayo interesante de verificación",
+                "implica el proceso de reestructuración y modernización", "habrá de significar un auténtico y eficaz punto de partida",
+                "permite en todo caso explicitar las razones fundamentales", "asegura, en todo caso, un proceso muy sensible de inversión",
+                "radica en una elaboración cuidadosa y sistemática de las estrategias adecuadas", "deriva de una indirecta incidencia superadora")
+        col4 = ("de las condiciones financieras y administrativas existentes.", "de las directivas de desarrollo para el futuro.",
+                "del sistema de participación general.", "de las actitudes de los miembros hacia sus deberes ineludibles.",
+                "de las nuevas proposiciones.", "de las direcciones educativas en el sentido del progreso.",
+                "del sistema de formación de cuadros que corresponda a las necesidades.", "de las condiciones de las actividades apropiadas.",
+                "del modelo de desarrollo.", "de las formas de acción.", "de las básicas premisas adoptadas.",
+                "de toda una casuística de amplio espectro.", "de los elementos generadores.",
+                "para configurar una interface amigable y coadyuvante a la reingeniería del sistema.",
+                "de toda una serie de criterios ideológicamente sistematizados en un frente común de actuación regeneradora.")
+
+        all = [col1, col2, col3, col4]
+
+        with open(filename, 'w') as f:
+            for i in range(linecount):
+                f.writelines([' '.join([random.choice(i) for i in all]), '\n'])
+        pass
+
+
+    def generate_pdf(self, filename_input, filename_output):
+        # save FPDF() class into
+        # a variable pdf
+        pdf = FPDF()
+
+        # Add a page
+        pdf.add_page()
+
+        # set style and size of font
+        # that you want in the pdf
+        pdf.set_font("Arial", size=10)
+
+        # open the text file in read mode
+        f = open(filename_input, "r")
+
+        # insert the texts in pdf
+        for x in f:
+            pdf.cell(200, 10, txt=x, ln=1, align='C')
+
+        # save the pdf with name .pdf
+        pdf.output(filename_output)
+        pass
+
+
+    def generate_img(self, filename_input, filename_output):
+        width = 1024
+        height = 512
+        message = ''
+
+        img = Image.open("./resources/watermark.png").resize((width, height))
+        imgDraw = ImageDraw.Draw(img)
+
+        f = open(filename_input, "r")
+        for x in f:
+            message = message + x
+        f.close()
+
+        font = ImageFont.truetype("arial.ttf", size=20)
+
+        imgDraw.text((10, 10), message, font=font, fill=(255, 255, 0))
+        img = img.convert('RGB')
+        img.save(filename_output)
+        pass
+
+    def generate_docx(self, filename_input, filename_output):
+        doc = Document()
+
+        with open(filename_input, 'r', encoding='latin-1') as openfile:
+            line = openfile.read()
+            doc.add_paragraph(line)
+            doc.save(filename_output)
+        pass
+
+    def generate_pdf_from_docx(self, filename_input, filename_output):
+        import win32com.client
+
+        wdFormatPDF = 17
+
+        inputFile = os.path.abspath(filename_input)
+        outputFile = os.path.abspath(filename_output)
+        word = win32com.client.Dispatch('Word.Application')
+        doc = word.Documents.Open(inputFile)
+        doc.SaveAs(outputFile, FileFormat=wdFormatPDF)
+        doc.Close()
+        word.Quit()
+        pass
+
+    def generate_avi(self, filename_avi, directory):
+        images = [img for img in os.listdir(directory) if img.endswith(".png")]
+        frame = cv2.imread(os.path.join(directory, images[0]))
+        height, width, layers = frame.shape
+
+        video = cv2.VideoWriter(filename_avi, 0, 1, (width,height))
+
+        for image in images:
+            video.write(cv2.imread(os.path.join(directory, image)))
+
+        cv2.destroyAllWindows()
+        video.release()
+        pass
+
+    def generate_audio(self, filename_input, filename_output):
+        from gtts import gTTS
+        from pydub import AudioSegment
+
+        file = open(filename_input, "r").read().replace("\n", " ")
+
+        language = "es"
+        region = "es"
+        speech = gTTS(text=str(file), lang=language, tld=region, slow=True)
+
+        speech.save(filename_output + ".mp3")
+        # playsound(audio_file)
+
+        sound1 = AudioSegment.from_file("./resources/base.mp3", format="mp3")
+        sound2 = AudioSegment.from_file(filename_output + ".mp3", format="mp3")
+
+        # Overlay sound2 over sound1 at position 0  (use louder instead of sound1 to use the louder version)
+        # overlay = sound1.overlay(sound2, position=0)
+        overlay = sound1.overlay(sound2, position=0.1 * len(sound1))
+
+        # simple export
+        file_handle = overlay.export(filename_output + " hit.mp3", format="mp3")
+
+        pass
---
+++ pyRandomFileGenerator.py
@@ -0,0 +1,138 @@
+import os
+import random
+import shutil
+import time
+import uuid
+
+import coloredlogs, logging
+import glob
+import configparser
+import datetime
+from modules.media import Media
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='INFO', logger=logger)
+
+global media
+
+configFilePath = "./config.ini"
+
+config = configparser.RawConfigParser(allow_no_value=True)
+config.read(configFilePath)
+
+def convert_bytes(bytes_number):
+    tags = ["Byte", "Kilobyte", "Megabyte", "Gigabyte", "Terabyte"]
+
+    i = 0
+    double_bytes = bytes_number
+
+    while (i < len(tags) and bytes_number >= 1024):
+        double_bytes = bytes_number / 1024.0
+        i = i + 1
+        bytes_number = bytes_number / 1024
+
+    return str(round(double_bytes, 2)) + " " + tags[i]
+
+def get_path_size(folderpath):
+    # assign size
+    size = 0
+
+    # get size
+    for path, dirs, files in os.walk(folderpath):
+        for f in files:
+            fp = os.path.join(path, f)
+            size += os.path.getsize(fp)
+
+    return size
+
+def create_file(extension, folder, filename):
+    txt_source = os.path.join(folder, filename + ".txt")
+    if bool(random.getrandbits(1)):
+        file_out = os.path.join(folder, (filename + "." + extension).upper())
+    else:
+        file_out = os.path.join(folder, (filename + "." + extension).lower())
+    if (extension == 'txt'):
+        media.generate_big_random_sentences(file_out, config.getint("extprop", "numSentences"))
+    elif (extension == 'docx'):
+        media.generate_docx(txt_source, file_out)
+    elif (extension == 'pdf'):
+        media.generate_pdf_from_docx(txt_source, file_out)
+    elif (extension == 'jpg' or extension == 'png'):
+        media.generate_img(txt_source, file_out)
+    elif (extension == 'mp3'):
+        try:
+            media.generate_audio(txt_source, file_out)
+        except:
+            logger.error("ERROR: Creando audio")
+    elif (extension == 'bin'):
+        media.generate_big_random_bin_file(file_out, config.getint("extprop", "sizeBIN"))
+    elif (extension == 'doc'):
+        media.generate_big_random_letters(file_out, config.getint("extprop", "sizeDOC"))
+    elif (extension == 'dat'):
+        media.generate_big_sparse_file(file_out, config.getint("extprop", "sizeDAT"))
+
+
+while True:
+
+    path_target = config.get("main", "pathTarget")
+
+    size = convert_bytes(get_path_size(path_target))
+    logger.info("### Tamaño directorio destino: " + size)
+
+    # Comprobar si el directorio de documentos se supera el tamaño esperado (3Gb)
+    if size.split()[1] == config.get("main", "localSizeUnit"):
+        if float(size.split()[0]) > config.getint("main", "localSize"):
+            logger.info("### Se ha superado el tamaño máximo: " + config.get("main", "localSize") + " " + config.get("main", "localSizeUnit"))
+            exit()
+
+    # Creamos varios los niveles de carpeta
+    for i in range(1, config.getint("main", "pathLevels")):
+        path_target = os.path.join(path_target, str(uuid.uuid4()))
+    path_target = os.path.join(path_target, str(uuid.uuid4()))
+    os.makedirs(path_target)
+    logger.info("### Directorio '% s' creado" % path_target)
+
+    #Generar documentos de forma aleatoria
+    if config.getboolean("files", "random"):
+        num_sentences = config.getint("extprop", "numSentences")
+        media = Media()
+
+        #Cremos los directorios por tipo
+        folder = {}
+        for extension in config.options("extension"):
+            if config.getboolean("extension", extension):
+                folder[extension] = os.path.join(path_target, extension + " " + str(uuid.uuid4()))
+                os.makedirs(folder[extension])
+
+        for i in range(1, config.getint("files", "num")+1):
+            filename = str(uuid.uuid4()) + " " + config.get("files", "name")
+
+            for extension in config.options("extension"):
+                if config.getboolean("extension", extension):
+                    create_file(extension, path_target, filename)
+
+            for extension in config.options("extension"):
+                if config.getboolean("extension", extension):
+                    for f in glob.glob(os.path.join(path_target, "*." + extension)):
+                        shutil.move(f, folder[extension])
+        # crear video
+        if config.getboolean("extension", "mp3"):
+            media.generate_avi(os.path.join(path_target, filename + ".avi"), os.path.join(folder['png']))
+
+    # Copiar documentos concretos en diferentes carpetas
+    else:
+        path_default = config.get("main", "pathSource")
+        files = os.listdir(path_default)
+        logger.info("### Documentos leidos" + str(files))
+
+        for filename in files:
+            split_tup = os.path.splitext(filename)
+            file_name = split_tup[0]
+            file_extension = split_tup[1]
+
+            shutil.copy(os.path.join(path_default, filename), os.path.join(path_target, file_name + ' ' +str(uuid.uuid4())+file_extension))
+
+    # Tiempo de espera para la siguiente ejecucion
+    time_wait = config.getint("main", "timeWaitToNextRun")
+    logger.info("### Proxima ejecucion: " + str(datetime.datetime.now() + datetime.timedelta(seconds=time_wait)))
+    time.sleep(time_wait)
